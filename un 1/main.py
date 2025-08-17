242f498bc3e7e07b6a304d901056d326cf833d93
rodrigoo730 <82415202+rodrigoo730@users.noreply.github.com>
2025-08-17 19:00:07 -0300

    Add files via upload

---
+++ un 1/main.py
@@ -0,0 +1,10 @@
+from tarjetaSube import tarjetaSube
+from test import test
+
+if __name__=='__main__':
+    print('hola')
+    test()
+    
+
+
+
---
+++ un 1/manejadorTarjeta.py
@@ -0,0 +1,5 @@
+class manejadorTarjeta:
+    __list=list
+
+    def __init__(self,list):
+        self.__list=list
